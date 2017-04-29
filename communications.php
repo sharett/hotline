93f8d88d83d3a1bd0ac6b29cda17b9f8e5b6fdd9
Bassam Kurdali <bassam@urchn.org>
2017-04-29 18:23:50 -0400

    auto refresh part 1

--- communications.php
+++ communications.php
@@ -6,6 +6,37 @@
 * Show the latest calls and texts in the $comms variable.
 * 
 */
+
+require_once 'config.php';
+require_once $LIB_BASE . 'lib_sms.php';
+
+// URL parameters
+$start = (int)$_REQUEST['s'];
+$mark = (int)$_REQUEST['mark'];
+$unmark = (int)$_REQUEST['unmark'];
+
+// Settings
+$page = 50;
+
+// Mark an item as responded, or not responded
+if ($mark) {
+	sms_markCommunication($mark, true /* mark responded */, $error);
+} else if ($unmark) {
+	sms_markCommunication($unmark, false /* mark not responded */, $error);
+}
+
+db_databaseConnect();
+// Communications
+$sql = "SELECT communications.*,contacts_from.contact_name AS from_contact, contacts_to.contact_name AS to_contact ".
+	"FROM communications ".
+	"LEFT JOIN contacts AS contacts_from ON contacts_from.phone = communications.phone_from ".
+	"LEFT JOIN contacts AS contacts_to ON contacts_to.phone = communications.phone_to ".
+	"ORDER BY communication_time DESC LIMIT ".addslashes($start).",{$page}";
+if (!db_db_query($sql, $comms, $error)) {
+    echo $error;
+}
+
+
 ?>
           <div class="table-responsive">
             <table class="table table-striped">
@@ -94,3 +125,7 @@ foreach ($comms as $comm) {
               </tbody>
             </table>
           </div>
+<?php
+
+db_databaseDisconnect();
+?>
