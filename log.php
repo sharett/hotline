dc97734234a8771f397fa812bdc8063f6e36a14b
Daniel Gilbert <loglow@gmail.com>
2017-05-06 20:02:16 -0400

    more of the same: putting error output into alert boxes

--- log.php
+++ log.php
@@ -35,7 +35,7 @@ $sql = "SELECT communications.*,contacts_from.contact_name AS from_contact, cont
 	"LEFT JOIN contacts AS contacts_to ON contacts_to.phone = communications.phone_to ".
 	"ORDER BY communication_time DESC LIMIT ".addslashes($start).",{$page}";
 if (!db_db_query($sql, $comms, $error)) {
-    echo $error;
+?><div class="alert alert-danger" role="alert"><?php echo $error ?></div><?php
 }
 
 ?>
