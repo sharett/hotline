fc7065af996852dee6d2a1f3b6360629bbe03e07
misplacedpriorities <change@aedificium.org>
2018-01-06 09:15:23 -0500

    Moved database update code into a function and added footer.

--- database_update.php
+++ database_update.php
@@ -65,116 +65,127 @@ function unlockTablesForDatabaseUpdate($previousError)
     return true;
 }
 
-// Lock the table.
-if (!db_db_command("LOCK TABLES call_times WRITE", $error)) {
-    displayError("The call times table could not updated because it ".
-            "could not be locked in the database.");
-    return;
-}
+/**
+* Perform any database updates required to bring the database up to date.
+*/
+function ensureDatabaseIsUpToDate()
+{
 
-// Query the database for any duplicate sets of entries. Such a set is
-// a set of entries that share the same contact, day, earliest, latest,
-// received texts/calls/call answered alerts, languages, and enabled
-// values.
-$query = "SELECT GROUP_CONCAT(id) AS ids, contact_id, day, earliest, latest, ".
-        "receive_texts, receive_calls, receive_call_answered_alerts, ".
-        "language_id, enabled, count(*) FROM call_times GROUP BY ".
-        "contact_id, day, earliest, latest, receive_texts, receive_calls, ".
-        "receive_call_answered_alerts, language_id, enabled ".
-        "HAVING count(*) > 1";
-if (!db_db_query($query, $duplicateResults, $error)) {
-    unlockTablesForDatabaseUpdate("The call times table could not be checked ".
-            "for duplicate entries because a database error occurred: ".$error);
-    return;
-}
-
-// If any duplicate entries are found, iterate through the sets of
-// their identifiers, compiling a list of all such identifiers in each
-// set besides the first one, and then delete all the entries associated
-// with the compiled identifiers.
-if (!empty($duplicateResults)) {
-    $duplicateIds = array();
-    foreach ($duplicateResults as $duplicateResult) {
-        $ids = explode(",", $duplicateResult["ids"]);
-        array_shift($ids);
-        $duplicateIds = array_merge($duplicateIds, $ids);
-    }
-    $sql = "DELETE FROM call_times WHERE id IN (".implode(",", $duplicateIds).")";
-    if (!db_db_command($sql, $error)) {
-        unlockTablesForDatabaseUpdate("The call times table could not have ".
-                "duplicate entries pruned because a database error occurred: ".
-                $error);
-        return;
-    }
-    $numDuplicates = count($duplicateIds);
-    if ($numDuplicates == 1) {
-        displaySuccess("The call times table had a single duplicate entry ".
-                "pruned.");
-    } else {
-        displaySuccess("The call times table had ".$numDuplicates." duplicate ".
-                "entries pruned.");
-    }
-}
-
-// Determine whether or not the entry_id column exists in the table, and
-// if it does not, create it and populate it.
-$query = "SHOW COLUMNS FROM call_times LIKE 'entry_id'";
-if (!db_db_getone($query, $results, $error)) {
-    unlockTablesForDatabaseUpdate("The call times table could not be checked ".
-            "for the entry_id column's existence because a database error ".
-            "occurred: ".$error);
-    return;
-}
-if (!$results) {
-
-    // Add the column to the table.
-    $sql = "ALTER TABLE call_times ADD entry_id int(11) UNSIGNED NOT NULL ".
-            "DEFAULT '0' AFTER id";
-    if (!db_db_command($sql, $error)) {
-        unlockTablesForDatabaseUpdate("The call times table could not have ".
-                "the entry_id column added because a database error occurred: ".
-                $error);
+    // Lock the table.
+    if (!db_db_command("LOCK TABLES call_times WRITE", $error)) {
+        displayError("The call times table could not updated because it ".
+                "could not be locked in the database.");
         return;
     }
 
-    // Find all the entries, grouped so that all entries that need the
-    // same entry identifier are together.
+    // Query the database for any duplicate sets of entries. Such a set is
+    // a set of entries that share the same contact, day, earliest, latest,
+    // received texts/calls/call answered alerts, languages, and enabled
+    // values.
     $query = "SELECT GROUP_CONCAT(id) AS ids, contact_id, day, earliest, latest, ".
             "receive_texts, receive_calls, receive_call_answered_alerts, ".
-            "enabled, count(*) FROM call_times GROUP BY contact_id, day, ".
-            "earliest, latest, receive_texts, receive_calls, ".
-            "receive_call_answered_alerts, enabled";
-    if (!db_db_query($query, $entryResults, $error)) {
-        unlockTablesForDatabaseUpdate("The call times table could not have ".
-                "the new entry identifier column populated because a ".
-                "database error occurred: ".$error);
+            "language_id, enabled, count(*) FROM call_times GROUP BY ".
+            "contact_id, day, earliest, latest, receive_texts, receive_calls, ".
+            "receive_call_answered_alerts, language_id, enabled ".
+            "HAVING count(*) > 1";
+    if (!db_db_query($query, $duplicateResults, $error)) {
+        unlockTablesForDatabaseUpdate("The call times table could not be checked ".
+                "for duplicate entries because a database error occurred: ".$error);
         return;
     }
 
-    // Iterate through the results, setting each group of records to use
-    // a common entry identifier.
-    if (!empty($entryResults)) {
-        $entryIdentifier = 1;
-        foreach ($entryResults as $entryResult) {
-            $sql = "UPDATE call_times SET entry_id = ".$entryIdentifier.
-                    " WHERE id IN (".$entryResult["ids"].")";
-            if (!db_db_command($sql, $error)) {
-                unlockTablesForDatabaseUpdate("The call times table could not ".
-                        "have the new entry identifier column populated ".
-                        "because a database error occurred: ".$error);
-                return;
-            }
-            $entryIdentifier++;
+    // If any duplicate entries are found, iterate through the sets of
+    // their identifiers, compiling a list of all such identifiers in each
+    // set besides the first one, and then delete all the entries associated
+    // with the compiled identifiers.
+    if (!empty($duplicateResults)) {
+        $duplicateIds = array();
+        foreach ($duplicateResults as $duplicateResult) {
+            $ids = explode(",", $duplicateResult["ids"]);
+            array_shift($ids);
+            $duplicateIds = array_merge($duplicateIds, $ids);
+        }
+        $sql = "DELETE FROM call_times WHERE id IN (".implode(",", $duplicateIds).")";
+        if (!db_db_command($sql, $error)) {
+            unlockTablesForDatabaseUpdate("The call times table could not have ".
+                    "duplicate entries pruned because a database error occurred: ".
+                    $error);
+            return;
+        }
+        $numDuplicates = count($duplicateIds);
+        if ($numDuplicates == 1) {
+            displaySuccess("The call times table had a single duplicate entry ".
+                    "pruned.");
+        } else {
+            displaySuccess("The call times table had ".$numDuplicates." duplicate ".
+                    "entries pruned.");
         }
     }
 
-    displaySuccess("The call times table had the entry identifier column ".
-            "added and populated for all existing entries.");
+    // Determine whether or not the entry_id column exists in the table, and
+    // if it does not, create it and populate it.
+    $query = "SHOW COLUMNS FROM call_times LIKE 'entry_id'";
+    if (!db_db_getone($query, $results, $error)) {
+        unlockTablesForDatabaseUpdate("The call times table could not be checked ".
+                "for the entry_id column's existence because a database error ".
+                "occurred: ".$error);
+        return;
+    }
+    if (!$results) {
+
+        // Add the column to the table.
+        $sql = "ALTER TABLE call_times ADD entry_id int(11) UNSIGNED NOT NULL ".
+                "DEFAULT '0' AFTER id";
+        if (!db_db_command($sql, $error)) {
+            unlockTablesForDatabaseUpdate("The call times table could not have ".
+                    "the entry_id column added because a database error occurred: ".
+                    $error);
+            return;
+        }
+
+        // Find all the entries, grouped so that all entries that need the
+        // same entry identifier are together.
+        $query = "SELECT GROUP_CONCAT(id) AS ids, contact_id, day, earliest, latest, ".
+                "receive_texts, receive_calls, receive_call_answered_alerts, ".
+                "enabled, count(*) FROM call_times GROUP BY contact_id, day, ".
+                "earliest, latest, receive_texts, receive_calls, ".
+                "receive_call_answered_alerts, enabled";
+        if (!db_db_query($query, $entryResults, $error)) {
+            unlockTablesForDatabaseUpdate("The call times table could not have ".
+                    "the new entry identifier column populated because a ".
+                    "database error occurred: ".$error);
+            return;
+        }
+
+        // Iterate through the results, setting each group of records to use
+        // a common entry identifier.
+        if (!empty($entryResults)) {
+            $entryIdentifier = 1;
+            foreach ($entryResults as $entryResult) {
+                $sql = "UPDATE call_times SET entry_id = ".$entryIdentifier.
+                        " WHERE id IN (".$entryResult["ids"].")";
+                if (!db_db_command($sql, $error)) {
+                    unlockTablesForDatabaseUpdate("The call times table could not ".
+                            "have the new entry identifier column populated ".
+                            "because a database error occurred: ".$error);
+                    return;
+                }
+                $entryIdentifier++;
+            }
+        }
+
+        displaySuccess("The call times table had the entry identifier column ".
+                "added and populated for all existing entries.");
+    }
+
+    // Unlock the table.
+    if (!unlockTablesForDatabaseUpdate(null)) {
+        return;
+    }
+
+    displaySuccess("Update completed successfully.");
 }
 
-// Unlock the table.
-if (!unlockTablesForDatabaseUpdate(null)) {
-    return;
-}
+ensureDatabaseIsUpToDate();
 
-displaySuccess("Update completed successfully.");
+include 'footer.php';
